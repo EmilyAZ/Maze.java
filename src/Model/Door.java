f65213bb0e8f33cab32167760b540c6d13a758e5
EmilyAZ <166950683+EmilyAZ@users.noreply.github.com>
2024-05-04 00:09:13 -0700

    iteration3

--- src/Model/Door.java
+++ src/Model/Door.java
@@ -2,13 +2,13 @@ package Model;
 public class Door {
     private boolean doorLocked;
 
-    public Door(boolean doorLocked) {
-        this.doorLocked = doorLocked;
+    public Door(final boolean theDoorLocked) {
+        doorLocked = theDoorLocked;
     }
     public boolean getDoorStatus(){
         return doorLocked;
     }
-    public void setDoorStatus(boolean doorLocked){
-        this.doorLocked = doorLocked;
+    public void setDoorStatus(final boolean theDoorLocked){
+        doorLocked = theDoorLocked;
     }
 }
--- src/tests/DoorTests.java
+++ src/tests/DoorTests.java
@@ -1,5 +1,28 @@
 package tests;
 
+import Model.Door;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 public class DoorTests {
+    private Door myDoor;
+    @Test
+    public void testConstructor(){
+        myDoor = new Door(true);
+        assertTrue(myDoor.getDoorStatus());
+    }
+    @Test
+    public void testGetDoor(){
+        myDoor = new Door(true);
+        assertTrue(myDoor.getDoorStatus());
+    }
+    @Test
+    public void testSetMyDoor(){
+        myDoor = new Door(true);
+        myDoor.setDoorStatus(false);
+        assertFalse(myDoor.getDoorStatus());
+    }
+
 
 }
