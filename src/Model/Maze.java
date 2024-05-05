537eb1e1fb223ff8ff6d86a4d5deb3be5c54129a
EmilyAZ <166950683+EmilyAZ@users.noreply.github.com>
2024-05-05 15:01:24 -0700

    iteration3

--- src/Model/Maze.java
+++ src/Model/Maze.java
@@ -1,27 +1,32 @@
 package Model;
 
+import java.awt.*;
+
 public class Maze {
-    private static final int MIN_ROOMS = 2;
+    private static final int MIN_ROOMS = 4;
     private static final int TWO_DOOR_ROOM = 2;
     private static final int THREE_DOOR_ROOM = 3;
     private static final int FOUR_DOOR_ROOM = 4;
-    private Room[][] myMaze;
-    private Room myEntrance;
-    private Room myExit;
-    private Room myCurrentRoom;
-    private int myMazeRows;
-    private int myMazeColumns;
+    private final Room[][] myMaze;
+    private final Point myExit;
+    private Point myCurrentRoom;
+    private final int myMazeRows;
+    private final int myMazeColumns;
 
     public Maze(final int theMazeRows, final int theMazeColumns) {
         if (theMazeRows < MIN_ROOMS || theMazeColumns < MIN_ROOMS) {
-            throw new IllegalArgumentException("There must be at least 2 rows and 2 columns of rooms in maze");
+            throw new IllegalArgumentException("There must be at least 4 rows and 4 columns of rooms in maze");
         } else {
             myMazeRows = theMazeRows;
             myMazeColumns = theMazeColumns;
         }
         myMaze = new Room[myMazeRows][myMazeColumns];
+        createMaze();
+        myCurrentRoom = mazeEntrance();
+        myExit = mazeExit();
     }
-    public void createMaze(){
+
+    private void createMaze(){
         for(int i = 0; i < myMazeRows; i++){
             for(int j = 0; j < myMazeColumns; j++){
                 if(((i == 0) && (j == 0 || j+1 ==myMazeColumns))
@@ -29,13 +34,33 @@ public class Maze {
                     myMaze[i][j] = new Room(TWO_DOOR_ROOM);
                 }
                 else if((i > 0 && i+1 < myMazeRows) && (j == 0 || j+1 == myMazeColumns)
-                        ||((i == 0 || i + 1 == myMazeRows) && (j > 0 && j +1 < myMazeColumns))){ //if a middle row and edge room
+                        ||((i == 0 || i + 1 == myMazeRows) && (j > 0 && j +1 < myMazeColumns))){ //if edge room not a corner
                     myMaze[i][j] = new Room(THREE_DOOR_ROOM);
                 }
                 else{
-                    myMaze[i][j] = new Room(FOUR_DOOR_ROOM);
+                    myMaze[i][j] = new Room(FOUR_DOOR_ROOM);// any room surrounded by other rooms
                 }
+
             }
         }
+    }
+    private Point mazeEntrance(){ //temporary entrance
+        return new Point(0,0);
+    }
+    private Point mazeExit(){ //temporary exit
+        return new Point(myMazeRows-1, myMazeColumns-1);
+    }
+    public void setCurrentRoom(final int theXCord, final int theYCord){
+        if(theXCord >= myMazeColumns || theXCord < 0 || theYCord >= myMazeRows || theYCord < 0){
+            throw new IllegalArgumentException("invalid coordinates out of bound for maze");
+        }
+        myCurrentRoom.x = theXCord;
+        myCurrentRoom.y = theYCord;
+    }
+    public Point getMyCurrentRoom(){
+        return myCurrentRoom;
+    }
+    public Point getMyExit(){
+        return myExit;
     }
 }
--- src/tests/MazeTests.java
+++ src/tests/MazeTests.java
@@ -1,16 +1,41 @@
 package tests;
 
-import Model.Door;
 import Model.Maze;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class MazeTests {
     private Maze myMaze;
     @Test
-    public void testConstructor(){
-        myMaze = new Maze(3,3);
-        myMaze.createMaze();
+    public void testConstructorMazeEntrance(){
+        myMaze = new Maze(4,4);
+        assertEquals(0, myMaze.getMyCurrentRoom().x);
+        assertEquals(0,myMaze.getMyCurrentRoom().y);
     }
+    @Test
+    public void testConstructorMazeExit(){
+        myMaze = new Maze(4,5);
+        assertEquals(3, myMaze.getMyExit().x);
+        assertEquals(4, myMaze.getMyExit().y);
+    }
+    @Test
+    public void testConstructorInvalidRoom(){
+        assertThrows(IllegalArgumentException.class, () ->myMaze = new Maze(2,3));
+    }
+    @Test
+    public void testSetCurrentRoom(){
+        myMaze = new Maze(5,5);
+        myMaze.setCurrentRoom(1,2);
+        assertEquals(1,myMaze.getMyCurrentRoom().x);
+        assertEquals(2,myMaze.getMyCurrentRoom().y);
+    }
+    @Test
+    public void testSetCurrentRoomInvalidInput(){
+        myMaze = new Maze(4,7);
+            assertThrows(IllegalArgumentException.class, () ->myMaze.setCurrentRoom(4,7));
+            assertThrows(IllegalArgumentException.class, () ->myMaze.setCurrentRoom(-1,7));
+
+    }
+
 }
